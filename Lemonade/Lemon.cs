bcd09aefe159ee970585e74088803c599612416a
Saša Barišić <cartman300@net.hr>
2017-11-14 13:01:57 +0100

    It works!

--- Lemonade/Lemon.cs
+++ Lemonade/Lemon.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+//using LemonPtr = System.IntPtr;
 
 namespace Lemonade {
-	
+	public struct LemonLang {
+		const string DllName = nameof(LemonLang);
+		const CallingConvention CConv = CallingConvention.Cdecl;
+		const CharSet CSet = CharSet.Ansi;
 
-	public class Lemon {
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet, SetLastError = true)]
+		public static extern void builtin_init(LemonPtr Lmn);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet, SetLastError = true)]
+		public static extern LemonPtr lemon_create();
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet, SetLastError = true)]
+		public static extern void lemon_machine_reset(LemonPtr Lmn);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet, SetLastError = true)]
+		public static extern int lemon_machine_execute(LemonPtr Lmn);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet, SetLastError = true)]
+		public static extern int lemon_input_set_file(LemonPtr Lmn, string FileName);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet, SetLastError = true)]
+		public static extern int lemon_input_set_buffer(LemonPtr Lmn, string FileName, string Buffer, int Len);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet, SetLastError = true)]
+		public static extern int lemon_compile(LemonPtr Lmn);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet, SetLastError = true)]
+		public static extern void lemon_destroy(LemonPtr Lmn);
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct LemonPtr {
+		public IntPtr Ptr;
 	}
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
+//using LemonPtr = System.IntPtr;
 using Lemonade;
 
 namespace Test {
 	class Program {
 		static void Main(string[] args) {
+			LemonPtr Lmn = LemonLang.lemon_create();
+			LemonLang.builtin_init(Lmn);
+			
+			if (LemonLang.lemon_input_set_file(Lmn, "Test.lm") == 0) {
+				Console.WriteLine("Could not read input file");
+				Exit();
+			}
 
-			Console.WriteLine("Done!");
+			if (LemonLang.lemon_compile(Lmn) == 0) {
+				Console.WriteLine("Syntax error");
+				Exit();
+			}
+
+			LemonLang.lemon_machine_reset(Lmn);
+			LemonLang.lemon_machine_execute(Lmn);
+
+			LemonLang.lemon_destroy(Lmn);
 			Console.ReadLine();
+		}
+
+		static void Exit() {
+			Console.ReadLine();
+			Environment.Exit(0);
 		}
 	}
 }
